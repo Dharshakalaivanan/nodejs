316962079943eb607d05e11c1bbe9393f22f26a4
dharsha <dharshabhooma@gmail.com>
2024-08-12 12:32:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/*console.log('Hello world!')
+
+// Variables and declarations
+
+/*var num = 2;
+num = "House";
+
+console.log(num)
+
+// Conditionals
+// if (true) {
+//     let animalName = "Lion";
+//     animalName = 55;
+//     console.log(animalName);
+// } else if (true) {
+
+// } else {
+    
+// }
+
+
+// ___________________________________________
+/**
+ * 
+ */
+
+// Scopes
+// -------------
+// if (true) {
+//     const animalName = "Lion";
+//     // animalName = 55;
+//     console.log(animalName);
+// }
+// console.log("Animal name outside condition:", animalName);
+
+// const personName = "John Doe";
+// personName = "Rita";
+// console.log(personName);
+
+// -----------------------------------
+
+let num=100;
+if(num%2==0)
+{
+    console.log("even number");
+}
+else{
+    console.log("odd number");
+}
